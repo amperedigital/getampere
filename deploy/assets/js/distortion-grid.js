349e9849471c8e0ace9f4bebb081f87f4e98c157
Anti-Gravity <antigravity@amperedigital.com>
2026-02-12 16:48:29 -0500

    chore(release): v2.989

--- deploy/assets/js/distortion-grid.js
+++ deploy/assets/js/distortion-grid.js
@@ -3,7 +3,7 @@
 // Version: v1.790
 
 (function() {
-console.log('[DistortionGrid] v2.988 Loaded'); 
+console.log('[DistortionGrid] v2.989 Loaded'); 
 
 class DistortionGrid {
     constructor(parentElement, index) {
--- deploy/assets/js/tech-demo-main.js
+++ deploy/assets/js/tech-demo-main.js
@@ -1,4 +1,4 @@
-console.log('[Tech Demo] v2.988 Unified Release');
+console.log('[Tech Demo] v2.989 Unified Release');
 import { TechDemoScene } from './tech-demo-scene.js';
 import { initCardExpander } from './card-expander.js';
 import { initAllSockets } from './glass-socket.js';
