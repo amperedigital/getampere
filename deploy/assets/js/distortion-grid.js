b872626c5fa622e85cff99375d6abf6430fedbab
Anti-Gravity <antigravity@amperedigital.com>
2026-02-12 17:30:22 -0500

    chore(release): v2.993

--- deploy/assets/js/distortion-grid.js
+++ deploy/assets/js/distortion-grid.js
@@ -3,7 +3,7 @@
 // Version: v1.790
 
 (function() {
-console.log('[DistortionGrid] v2.991 Loaded'); 
+console.log('[DistortionGrid] v2.993 Loaded'); 
 
 class DistortionGrid {
     constructor(parentElement, index) {
