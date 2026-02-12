5a302fd2b9a270d69edd053eb696934dd145bbea
Anti-Gravity <antigravity@amperedigital.com>
2026-02-12 18:13:25 -0500

    chore(release): v2.998

--- deploy/assets/js/ampere-3d-key.js
+++ deploy/assets/js/ampere-3d-key.js
@@ -7,7 +7,7 @@ export class Ampere3DKey {
         this.height = container.clientHeight;
         
         // Unified Ink - #1e2a40
-        console.log("Ampere3DKey v2.997 Loaded (Unified Ink - #1e2a40)");
+        console.log("Ampere3DKey v2.998 Loaded (Unified Ink - #1e2a40)");
 
         // State
         this.progress = 0;
--- deploy/assets/js/distortion-grid.js
+++ deploy/assets/js/distortion-grid.js
@@ -3,7 +3,7 @@
 // Version: v1.790
 
 (function() {
-console.log('[DistortionGrid] v2.997 Loaded'); 
+console.log('[DistortionGrid] v2.998 Loaded'); 
 
 class DistortionGrid {
     constructor(parentElement, index) {
