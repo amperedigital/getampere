6b9235c422bb148a35b4982c1e62c23b5b18217a
Anti-Gravity <antigravity@amperedigital.com>
2026-02-13 17:22:52 -0500

    chore(release): v3.012

--- deploy/assets/js/scroll-flipper.js
+++ deploy/assets/js/scroll-flipper.js
@@ -8,7 +8,7 @@
  */
 
 (function() {
-    console.log('[ScrollFlipper v3.011] Loading High-Performance Mode...');
+    console.log('[ScrollFlipper v3.012] Loading High-Performance Mode...');
 
     let isRunning = false;
     let track, stickyContainer, cards, triggers, cardParent;
