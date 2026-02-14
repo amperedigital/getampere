c709043dc5e5a52f3fba8e14f6785d632b281dac
Anti-Gravity <antigravity@amperedigital.com>
2026-02-14 15:26:09 -0500

    chore(release): v3.031

--- deploy/assets/js/ampere-3d-key.js
+++ deploy/assets/js/ampere-3d-key.js
@@ -7,7 +7,7 @@ export class Ampere3DKey {
         this.height = container.clientHeight;
         
         // Unified Ink - #1e2a40
-        console.log("Ampere3DKey v3.030 Loaded (Unified Ink - #1e2a40)");
+        console.log("Ampere3DKey v3.031 Loaded (Unified Ink - #1e2a40)");
 
         // State
         this.progress = 0;
--- deploy/assets/js/scroll-flipper.js
+++ deploy/assets/js/scroll-flipper.js
@@ -8,7 +8,7 @@
  */
 
 (function() {
-    console.log('[ScrollFlipper v3.030] Loading High-Performance Mode...');
+    console.log('[ScrollFlipper v3.031] Loading High-Performance Mode...');
 
     let isRunning = false;
     let track, stickyContainer, cards, triggers, cardParent;
