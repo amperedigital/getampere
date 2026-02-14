ee930c95fb056bb8750232c82f0c50a7051255ae
Anti-Gravity <antigravity@amperedigital.com>
2026-02-14 14:04:33 -0500

    chore(release): v3.021

--- deploy/assets/js/distortion-grid.js
+++ deploy/assets/js/distortion-grid.js
@@ -3,7 +3,7 @@
 // Version: v1.790
 
 (function() {
-console.log('[DistortionGrid] v3.020 Loaded'); 
+console.log('[DistortionGrid] v3.021 Loaded'); 
 
 class DistortionGrid {
     constructor(parentElement, index) {
--- deploy/assets/js/scroll-flipper.js
+++ deploy/assets/js/scroll-flipper.js
@@ -8,7 +8,7 @@
  */
 
 (function() {
-    console.log('[ScrollFlipper v3.020] Loading High-Performance Mode...');
+    console.log('[ScrollFlipper v3.021] Loading High-Performance Mode...');
 
     let isRunning = false;
     let track, stickyContainer, cards, triggers, cardParent;
