08b93b63117ed91e115c64a7a243fb3f24794310
Anti-Gravity <antigravity@amperedigital.com>
2026-02-19 14:07:17 -0500

    chore(release): v3.196

--- deploy/assets/js/ampere-3d-key.js
+++ deploy/assets/js/ampere-3d-key.js
@@ -7,7 +7,7 @@ export class Ampere3DKey {
         this.height = container.clientHeight;
         
         // Unified Ink - #1e2a40
-        console.log("Ampere3DKey v3.195-test Loaded (Unified Ink - #1e2a40)");
+        console.log("Ampere3DKey v3.196-test Loaded (Unified Ink - #1e2a40)");
 
         // State
         this.progress = 0;
