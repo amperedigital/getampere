67ebe0bbeae6873b86a62937e0a90bfd6192a733
Anti-Gravity <antigravity@amperedigital.com>
2026-02-21 14:49:30 -0500

    chore(release): v3.208

--- deploy/assets/js/scroll-flipper.js
+++ deploy/assets/js/scroll-flipper.js
@@ -8,7 +8,7 @@
  */
 
 (function() {
-    console.log('[ScrollFlipper v3.206-test] Loading High-Performance Mode...');
+    console.log('[ScrollFlipper v3.208-test] Loading High-Performance Mode...');
 
     let isRunning = false;
     let track, stickyContainer, cards, triggers, cardParent;
--- deploy/assets/js/tech-demo-main.js
+++ deploy/assets/js/tech-demo-main.js
@@ -1,4 +1,4 @@
-console.log('[Tech Demo] v3.206 Multi-Agent Visualization');
+console.log('[Tech Demo] v3.208 Multi-Agent Visualization');
 import { TechDemoScene } from './tech-demo-scene.js';
 import { initCardExpander } from './card-expander.js';
 import { initAllSockets } from './glass-socket.js';
