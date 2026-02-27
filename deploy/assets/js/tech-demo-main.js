64f6d48beee1201b56b195fc1c51d31960466b84
Anti-Gravity <antigravity@amperedigital.com>
2026-02-27 13:32:53 -0500

    chore(release): v3.307

--- deploy/assets/js/tech-demo-main.js
+++ deploy/assets/js/tech-demo-main.js
@@ -1,4 +1,4 @@
-console.log('[Tech Demo] v3.306 Multi-Agent Visualization');
+console.log('[Tech Demo] v3.307 Multi-Agent Visualization');
 import { TechDemoScene } from './tech-demo-scene.js';
 import { initCardExpander } from './card-expander.js';
 import { initAllSockets } from './glass-socket.js';
