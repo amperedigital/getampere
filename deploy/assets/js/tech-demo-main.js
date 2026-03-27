d7566a4d9abb60c5031213ce237ec62129a1b067
Anti-Gravity <antigravity@amperedigital.com>
2026-03-27 14:57:40 -0400

    chore(release): v3.735

--- deploy/assets/js/tech-demo-main.js
+++ deploy/assets/js/tech-demo-main.js
@@ -1,4 +1,4 @@
-console.log('[Tech Demo] v3.734 Voice Pipe Web Channel');
+console.log('[Tech Demo] v3.735 Voice Pipe Web Channel');
 import { TechDemoScene } from './tech-demo-scene.js';
 import { initCardExpander } from './card-expander.js';
 import { initAllSockets } from './glass-socket.js';
